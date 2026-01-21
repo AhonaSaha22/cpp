fbfb78c90112f033b3960a2dc321c49ea0bc945f
Ahona Saha <sahaahona4@gmail.com>
2026-01-21 20:15:50 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,148 +1,144 @@
 #include <iostream>
-#include <stack>
+#include <queue>
 using namespace std;
 
-struct Student {
+struct Book {
     int id;
-    string name;
-    float cgpa;
+    string name ;
+    bool issued ;
 };
 
-Student s[100];
-int n = 0;
-stack<Student> undoStack;
+struct Node {
+    Book data;
+    Node* next ;
+};
 
-void addStudent() {
-    cout << "Enter ID: ";
-    cin >> s[n].id;
-    cout << "Enter Name: ";
+Node* head = NULL;
+queue<int> waitQ ;
+
+void addBook() {
+    Book b;
+    cout << "Enter Book ID: " ;
+    cin >> b.id;
+    cout << "Enter Book Name: " ;
     cin.ignore();
-    getline(cin, s[n].name);
-    cout << "Enter CGPA: ";
-    cin >> s[n].cgpa;
-    n++;
-    cout << "Student added successfully!\n";
+    getline(cin, b.name);
+    b.issued = false ;
+
+    Node* n = new Node ;
+    n->data = b;
+    n->next = head;
+    head = n;
+
+    cout << "Book added successfully!\n";
 }
 
-void displayStudents() {
-    if (n == 0) {
-        cout << "No students found!\n";
+void displayBooks() {
+    if (head == NULL) {
+        cout << "No books available!\n"  ;
         return;
     }
-    cout << "\nID\tName\tCGPA\n";
-    for (int i = 0; i < n; i++) {
-        cout << s[i].id << "\t"
-             << s[i].name << "\t"
-             << s[i].cgpa << endl;
+
+    Node* temp = head;
+    cout << "\nID\tName\t\tStatus\n" ;
+    while (temp != NULL) {
+        cout << temp->data.id << "\t"
+             << temp->data.name << "\t"
+             << (temp->data.issued ? "Issued" : "Available") << endl;
+        temp = temp->next;
     }
 }
 
-void searchStudent() {
+Node* searchBook(int id) {
+    Node* temp = head;
+    while (temp != NULL) {
+        if (temp->data.id == id)
+            return temp;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+void issueBook() {
     int id;
-    cout << "Enter ID to search: ";
-    cin >> id;
-    for (int i = 0; i < n; i++) {
-        if (s[i].id == id) {
-            cout << "Found: " << s[i].name
-                 << " | CGPA: " << s[i].cgpa << endl;
-            return;
-        }
-    }
-    cout << "Student not found!\n";
-}
-
-void deleteStudent() {
-    int id, pos = -1;
-    cout << "Enter ID to delete: ";
+    cout << "Enter Book ID to issue: ";
     cin >> id;
 
-    for (int i = 0; i < n; i++) {
-        if (s[i].id == id) {
-            pos = i;
-            break;
-        }
-    }
-
-    if (pos == -1) {
-        cout << "Student not found!\n";
+    Node* b = searchBook(id);
+    if (b == NULL) {
+        cout << "Book not found!\n";
         return;
     }
 
-    undoStack.push(s[pos]);
-
-    for (int i = pos; i < n - 1; i++) {
-        s[i] = s[i + 1];
+    if (b->data.issued) {
+        cout << "Book already issued. Added to waiting queue.\n";
+        waitQ.push(id);
+    } else {
+        b->data.issued = true;
+        cout << "Book issued successfully!\n";
     }
-    n--;
-
-    cout << "Student deleted (Undo available).\n";
 }
 
-void undoDelete() {
-    if (undoStack.empty()) {
-        cout << "Nothing to undo!\n";
+void returnBook() {
+    int id;
+    cout << "Enter Book ID to return: ";
+    cin >> id;
+
+    Node* b = searchBook(id);
+    if (b == NULL || b->data.issued == false) {
+        cout << "Invalid return!\n";
         return;
     }
 
-    s[n] = undoStack.top();
-    undoStack.pop();
-    n++;
-
-    cout << "Undo successful!\n";
-}
-
-void sortByCGPA() {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (s[i].cgpa < s[j].cgpa) {
-                swap(s[i], s[j]);
-            }
-        }
+    if (!waitQ.empty()) {
+        int next = waitQ.front();
+        waitQ.pop();
+        cout << "Book issued to next request (ID: " << next << ")\n";
+    } else {
+        b->data.issued = false;
+        cout << "Book returned successfully!\n";
     }
-    cout<< "Sorted by CGPA (Descending).\n";
 }
+
 int main() {
     int choice;
 
-    do {
-        cout << "\n--- Student Management System ---\n";
-        cout << " 1. Add Student\n"  ;
-        cout << " 2. Display Students\n"  ;
-        cout << "3. Search Student\n" ;
-        cout << "4. Delete Student\n";
-        cout << "5. Undo Delete\n";
-        cout << "6. Sort by CGPA\n"  ;
+    while (true) {
+        cout << "\n Library Management System-\n";
+        cout << "1. Add Book\n";
+        cout << "2. Display the Books\n" ;
+        cout << "3. Search Book\n" ;
+        cout << "4. Issue Book\n"  ;
+        cout << "5. Return Book\n";
         cout << "0. Exit\n";
-        cout << "Enter choice: " ;
+        cout << "Enter choice: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                 addStudent();
-                  break;
-            case 2:
-                 displayStudents();
-                 break;
-            case 3:
-                 searchStudent();
-                 break;
-            case 4:
-                deleteStudent();
-                 break;
-            case 5:
-                 undoDelete();
-                  break;
-            case 6:
-                sortByCGPA();
-                break;
-            case 0:
-                cout << "Exiting...\n";
-                break;
-            default: cout << "Invalid choice!\n";
+        if (choice == 1)
+            addBook();
+        else if (choice == 2)
+            displayBooks();
+        else if (choice == 3) {
+            int id;
+            cout << "Enter Book ID: ";
+            cin >> id;
+            Node* b = searchBook(id);
+            if (b)
+                cout << "Found: "<< b->data.name << endl;
+            else
+                cout << "Book not found!\n";
         }
-
+        else if (choice==4)
+            issueBook();
+        else if (choice==  5)
+            returnBook();
+        else if (choice== 0) {
+            cout<<"Exit\n";
+            break;
+        }
+        else
+            cout<<"Invalid choice!\n";
     }
-     while (choice!=0);
 
     return 0;
 }
