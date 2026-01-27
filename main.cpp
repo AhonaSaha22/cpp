6a23c8dbb2822f2f3ba34f3f61d6173baf5fd39c
Ahona Saha <sahaahona4@gmail.com>
2026-01-28 01:52:50 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,143 +1,82 @@
 #include <iostream>
-#include <queue>
+#include <string>
+#include <vector>
 using namespace std;
+   double calculate() {
+     double a,b  ;
+      char c;
 
-struct Book {
-    int id;
-    string name ;
-    bool issued ;
-};
+    cout<<"Enter math expression ( e.g., 5 + 3): ";
+    cin>>a >> c>> b;
+    cin.ignore(1000, '\n');
 
-struct Node {
-    Book data;
-    Node* next ;
-};
-
-Node* head = NULL;
-queue<int> waitQ ;
-
-void addBook() {
-    Book b;
-    cout << "Enter Book ID: " ;
-    cin >> b.id;
-    cout << "Enter Book Name: " ;
-    cin.ignore();
-    getline(cin, b.name);
-    b.issued = false ;
-
-    Node* n = new Node ;
-    n->data = b;
-    n->next = head;
-    head = n;
-
-    cout << "Book added successfully!\n";
+    if(c== '+') return a + b;
+    else if(c == '-') return a - b;
+    else if(c== '*') return a * b;
+    else if(c== '/') return (b != 0) ? a / b : 0;
+    else return 0;
 }
-
-void displayBooks() {
-    if (head == NULL) {
-        cout << "No books available!\n"  ;
-        return;
-    }
-
-    Node* temp = head;
-    cout << "\nID\tName\t\tStatus\n" ;
-    while (temp != NULL) {
-        cout << temp->data.id << "\t"
-             << temp->data.name << "\t"
-             << (temp->data.issued ? "Issued" : "Available") << endl;
-        temp = temp->next;
-    }
-}
-
-Node* searchBook(int id) {
-    Node* temp = head;
-    while (temp != NULL) {
-        if (temp->data.id == id)
-            return temp;
-        temp = temp->next;
-    }
-    return NULL;
-}
-
-void issueBook() {
-    int id;
-    cout << "Enter Book ID to issue: ";
-    cin >> id;
-
-    Node* b = searchBook(id);
-    if (b == NULL) {
-        cout << "Book not found!\n";
-        return;
-    }
-
-    if (b->data.issued) {
-        cout << "Book already issued. Added to waiting queue.\n";
-        waitQ.push(id);
-    } else {
-        b->data.issued = true;
-        cout << "Book issued successfully!\n";
-    }
-}
-
-void returnBook() {
-    int id;
-    cout << "Enter Book ID to return: ";
-    cin >> id;
-
-    Node* b = searchBook(id);
-    if (b == NULL || b->data.issued == false) {
-        cout << "Invalid return!\n";
-        return;
-    }
-
-    if (!waitQ.empty()) {
-        int next = waitQ.front();
-        waitQ.pop();
-        cout << "Book issued to next request (ID: " << next << ")\n";
-    } else {
-        b->data.issued = false;
-        cout << "Book returned successfully!\n";
-    }
-}
-
 int main() {
-    int choice;
+    string input, name;
+    vector<string> history;
+    bool hasName = false;
+
+    cout << "Welcome to Mini Chatbot!\n";
+cout << "\n-- Menu!!\n";
+    cout << "1. Greet\n2. Set Name\n3. Chat\n4. Math\n5. Show History\n6. Exit\n";
 
     while (true) {
-        cout << "\n Library Management System-\n";
-        cout << "1. Add Book\n";
-        cout << "2. Display the Books\n" ;
-        cout << "3. Search Book\n" ;
-        cout << "4. Issue Book\n"  ;
-        cout << "5. Return Book\n";
-        cout << "0. Exit\n";
-        cout << "Enter choice: ";
-        cin >> choice;
+        cout << "\nChoose option: ";
+        getline(cin, input);
 
-        if (choice == 1)
-            addBook();
-        else if (choice == 2)
-            displayBooks();
-        else if (choice == 3) {
-            int id;
-            cout << "Enter Book ID: ";
-            cin >> id;
-            Node* b = searchBook(id);
-            if (b)
-                cout << "Found: "<< b->data.name << endl;
-            else
-                cout << "Book not found!\n";
+        if(input == "1") {
+            cout << "Chatbot: Hello! Nice to meet you \n";
+            history.push_back("Greeted user");
         }
-        else if (choice==4)
-            issueBook();
-        else if (choice==  5)
-            returnBook();
-        else if (choice== 0) {
-            cout<<"Exit\n";
+        else if(input == "2") {
+            cout << "Chatbot: What is your name? ";
+            getline(cin, name);
+            cout << "Chatbot: Nice to meet you,"<< name << "!\n";
+            hasName= true;
+
+            history.push_back("Set name: " + name);
+        }
+        else if(input == "3") {
+            cout << "Chatbot: Say something: ";
+            getline(cin, input);
+            if(input == "how are you") cout << "Chatbot: I'm doing great! \n";
+            else if(input == "what is my name") {
+
+                if(hasName)
+                    cout << "Chatbot: Your name is " << name << "\n";
+
+                else cout << "Chatbot: I don't know your name yet \n";
+            }
+            else
+
+
+                cout<<"Chatbot:Sorry, I don't understand \n";
+
+            history.push_back("Chat: " +input);
+        }
+        else if(input == "4") {
+            double result = calculate();
+            cout << "Chatbot: Result = " <<result << "\n";
+            history.push_back("Math calculation done");
+        }
+        else if(input == "5") {
+            cout << "Chatbot: Chat History:\n";
+            for(int i = 0; i < history.size(); i++)
+                cout << i+1<< ". " << history[i] << "\n";
+        }
+        else if(input== "6") {
+            cout<< "Chatbot: Goodbye! \n";
+
             break;
         }
-        else
-            cout<<"Invalid choice!\n";
+        else {
+            cout<<"Chatbot: Invalid option \n";
+        }
     }
 
     return 0;
